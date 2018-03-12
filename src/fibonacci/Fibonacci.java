0c459d46307d41d576cceafb07b6b1fd79b1dcb3
lazarod <lazarod@SI2-10.uninorteacad.uninorte.edu.co>
2018-03-12 15:37:05 -0500

    Listo

--- src/fibonacci/Fibonacci.java
+++ src/fibonacci/Fibonacci.java
@@ -5,6 +5,9 @@
  */
 package fibonacci;
 
+import java.math.BigInteger;
+import java.util.Scanner;
+
 /**
  *
  * @author Estudiantes
@@ -15,7 +18,26 @@ public class Fibonacci {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        
+        Scanner sc = new Scanner(System.in);
+        BigInteger n;
+        System.out.println("Digite el numero");
+        n = sc.nextBigInteger();
+        System.out.println(fibonacci(n));
+    }
+    
+    public static BigInteger fibonacci(BigInteger n){
+        if(n.compareTo(BigInteger.ONE) <= 0){
+            return n;
+        }
+        BigInteger fib = new BigInteger("1");
+        BigInteger ant = new BigInteger("1");
+        BigInteger tem;
+        for(BigInteger i = (new BigInteger("2")); i.compareTo(n) < 0; i = i.add(BigInteger.ONE)){
+            tem = fib;
+            fib = fib.add(ant);
+            ant = tem;
+        }
+        return fib;
     }
     
 }
